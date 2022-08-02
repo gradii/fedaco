6f7cb924ffc2fbb46c6fe78caa196d36748cb76b
LinBo Len <linbolen@gradii.com>
2022-08-02 20:13:33 +0800

    feat: add doc site

---
+++ apps/doc-site/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fedaco-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'doc-site';
+}
